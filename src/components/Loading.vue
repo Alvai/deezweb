0c0dea47e77ace36316c7c20b84ef20f0907eeaa
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-28 21:54:53 +0100

    Update style

---
+++ src/components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img src="../assets/lave.svg" alt="loading..."/>
+    <h4>Loading...</h4>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "Loading"
+});
+</script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="search">
-    Search
+    <h3>Search</h3>
+    <div class="song-list">
+      <Loading v-if="loading" />
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
+import Loading from "@/components/Loading.vue";
 export default Vue.extend({
-  name: "search"
+  name: "search",
+  components: {
+    Loading
+  },
+  data() {
+    return {
+      loading: true
+    };
+  }
 });
 </script>
-<style>
-</style>
