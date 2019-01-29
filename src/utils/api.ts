396b9bff1b8e8fed240a4b94cb5727466859575e
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-29 12:32:02 +0100

    Update url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const CORS_ANYWHERE_URL: string = "https://horizon-zero-cors.herokuapp.com/";
-const API_URL: string = "http://api.bethesleak.ouch";
+const API_URL: string = "https://api.deezer.com";
 
 const api: AxiosInstance = axios.create({
   baseURL: `${CORS_ANYWHERE_URL}${API_URL}`
