78334578d0e4f56f88ee7519930e68d6fbcd6e12
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-29 18:54:03 +0100

    Add Favorite handling

--- src/utils/trackService.ts
+++ src/utils/trackService.ts
@@ -2,10 +2,48 @@ import api from "./api";
 import { SearchBar } from "../interfaces";
 import { DeezerTrack } from '@/interfaces';
 
+const LOCAL_STORAGE_KEY = "favoriteTracks";
+
 const searchTracks = async (searchBar: SearchBar): Promise<Array<DeezerTrack>> => {
   const { content, order } = searchBar;
   const res = await api.get(`/search?q=${content}&order=${order}_DESC`);
   return res.data.data;
 };
 
-export { searchTracks };
+const loadFavorites = (): Array<DeezerTrack> => {
+  if (!localStorage.getItem(LOCAL_STORAGE_KEY)) return [];
+  const favorites = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)!);
+  return favorites;
+};
+
+const setFavorites = (tracks: Array<DeezerTrack>): void => {
+  const json = JSON.stringify(tracks);
+  localStorage.setItem(LOCAL_STORAGE_KEY, json);
+}
+
+const addFavorite = (track: DeezerTrack): void => {
+  const favorites = loadFavorites();
+  favorites.push(track);
+  setFavorites(favorites);
+};
+
+const isFavorite = (track: DeezerTrack): boolean => {
+  const favorites = loadFavorites();
+  const filtered = favorites.filter(favoriteTrack => favoriteTrack.id === track.id);
+  return filtered.length > 0;
+};
+
+const removeFavorite = (track: DeezerTrack): void => {
+  const favorites = loadFavorites();
+  const updatedFavorites = favorites.filter(favoriteTrack => favoriteTrack.id !== track.id);
+  setFavorites(updatedFavorites);
+};
+
+export {
+  searchTracks,
+  loadFavorites,
+  addFavorite,
+  setFavorites,
+  removeFavorite,
+  isFavorite
+};
