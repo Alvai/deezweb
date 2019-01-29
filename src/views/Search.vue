be77d5e1b043c288d3bcb8810b0619f81b3433da
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-29 12:36:38 +0100

    Add searchTracks and loading

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="search">
     <h3>Search</h3>
-    <div class="song-list">
-      <Loading v-if="loading" />
-    </div>
+    <SearchForm @search="setTracks" @loading="loadingTracks"/>
+    <Loading :loading="loading" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from "vue";
 import Loading from "@/components/Loading.vue";
+import SearchForm from "@/components/SearchForm.vue";
+import { DeezerTrack } from "@/interfaces";
 export default Vue.extend({
   name: "search",
   components: {
-    Loading
+    Loading,
+    SearchForm,
   },
   data() {
     return {
-      loading: true
+      loading: false as boolean,
+      tracks: [] as Array<DeezerTrack>
     };
+  },
+  methods: {
+    setTracks(tracks: Array<DeezerTrack>) {
+      this.tracks = tracks;
+    },
+    loadingTracks(loading: boolean) {
+      this.loading = loading;
+    }
   }
 });
 </script>
