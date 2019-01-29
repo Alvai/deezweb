5319e0a1ebc86116adc0012723a5b7ee4a61b2d3
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-30 00:59:51 +0100

    Add random favorite display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="home">
-    Home
+    <h2>Home</h2>
+    <div v-if="favorites.length > 0">
+      <Loading :loading="loading" />
+      <Track v-if="!loading" :track="randomTrack" @dislike="getTrack" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { DeezerTrack } from '@/interfaces';
+import Track from '@/components/Track.vue';
+import Loading from '@/components/Loading.vue';
+import { loadFavorites, getRandomFavoriteTrack } from "../utils/trackService";
 
 export default Vue.extend({
-  name: "home"
+  name: "home",
+  components: {
+    Track,
+    Loading
+  },
+  data() {
+    return {
+      favorites: [] as Array<DeezerTrack>,
+      randomTrack: {} as DeezerTrack,
+      loading: false as boolean
+    };
+  },
+  methods: {
+    getTrack() {
+      this.loading = true;
+      this.randomTrack = getRandomFavoriteTrack();
+      this.loading = false;
+    }
+  },
+  created() {
+    this.favorites = loadFavorites();
+    this.getTrack(); 
+  }
 });
 </script>
