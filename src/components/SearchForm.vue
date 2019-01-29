ad397b8a826d60c3afd7c2d8a7d4d8e6f10ff363
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-29 12:31:54 +0100

    Create searchForm component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form @submit.prevent="getSongs(search)">
+      <input type="text" v-model="search.content" required>
+      <select v-model="search.order" required>
+        <option disabled value="">Please select one</option>
+        <option value="ALBUM">Album</option>
+        <option value="ARTIST">Artiste</option>
+        <option value="TRACK">Musique</option>
+        <option value="RANKING">Les plus populaires</option>
+        <option value="RATING">Les mieux notés</option>
+      </select>
+      <input type="submit" value="Chercher">
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { searchSongs } from "../utils/songService";
+import { SearchBar } from "@/interfaces";
+export default Vue.extend({
+  name: "SearchForm",
+  data() {
+    return {
+      search: {
+        content: "" as string,
+        order: "" as string
+      }
+    };
+  },
+  methods: {
+    async getSongs(search: SearchBar) {
+      if (this.search.content && this.search.order) {
+        this.$emit("loading", true);
+        const tracks = await searchSongs(search);
+        this.$emit("search", tracks);
+        this.$emit("loading", false);
+      }
+    }
+  }
+});
+</script>
