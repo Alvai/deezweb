5aebbc7affacda1601b0caea49b7ec555c0c3990
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-30 10:24:37 +0100

    add list update && css

--- src/components/TrackList.vue
+++ src/components/TrackList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="track-list">
-    <Track v-for="track in tracks" :key="track.id" :track="track" />
+    <Track v-for="track in tracks" :key="track.id" :track="track" @dislike="updateList"/>
   </div>
 </template>
 
@@ -17,6 +17,11 @@ export default Vue.extend({
     tracks: {
       type: Array as () => Array<DeezerTrack>
     }
+  },
+  methods: {
+    updateList() {
+      this.$emit('listUpdate');
+    }
   }
 });
 </script>
@@ -25,7 +30,7 @@ export default Vue.extend({
   .track-list {
     margin-top: 1vh;
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
     grid-gap: 25px;
     .track {
       max-height: 100%;
