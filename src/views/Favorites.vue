3a679ad926d2b1a5df1d64793510e5628427b1d2
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-30 10:22:07 +0100

    Add favorite display

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is the favorites page</h1>
+    <h1>Mes Favoris</h1>
+    <Loading :loading="loading" />
+    <TrackList v-if="favorites.length > 0" :tracks="favorites" @listUpdate="getFavorites"/>
+    <p v-else-if="!loading && favorites.length === 0">Pas de favoris</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { loadFavorites } from '@/utils/trackService';
+import { DeezerTrack } from '@/interfaces';
+import Loading from "@/components/Loading.vue";
+import TrackList from "@/components/TrackList.vue";
+export default Vue.extend({
+  name: "favorite",
+  components: {
+    Loading,
+    TrackList
+  },
+  data() {
+    return {
+      favorites: [] as Array<DeezerTrack>,
+      loading: true as boolean
+    }
+  },
+  methods: {
+    getFavorites() {
+      this.favorites = loadFavorites();
+    }
+  },
+  created() {
+    this.getFavorites();
+    this.loading = false;
+  }
+})
+</script>
