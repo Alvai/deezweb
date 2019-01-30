09f6b926848ceb816ce68499e30d7a2b2c81e306
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-01-30 11:47:30 +0100

    Déploiement du projet sur Github Pages

---
+++ precache-manifest.98c3e6a47b6ee54a1c47cbf8403f9ade.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "edbc8e304c3e3ee9385a",
+    "url": "/deezweb/css/app.14e2e215.css"
+  },
+  {
+    "revision": "edbc8e304c3e3ee9385a",
+    "url": "/deezweb/js/app.291fd9e2.js"
+  },
+  {
+    "revision": "d1b293d6a0d900205766",
+    "url": "/deezweb/css/chunk-vendors.6fc73b13.css"
+  },
+  {
+    "revision": "d1b293d6a0d900205766",
+    "url": "/deezweb/js/chunk-vendors.d5c0a7af.js"
+  },
+  {
+    "revision": "3e2507d60f466957d212",
+    "url": "/deezweb/js/home.88e6478a.js"
+  },
+  {
+    "revision": "7756775ffaf65540c1e2",
+    "url": "/deezweb/css/home~search~track.a9c04f83.css"
+  },
+  {
+    "revision": "7756775ffaf65540c1e2",
+    "url": "/deezweb/js/home~search~track.f5aeaf5d.js"
+  },
+  {
+    "revision": "d8441f467e85979ead54",
+    "url": "/deezweb/css/search.c3223302.css"
+  },
+  {
+    "revision": "d8441f467e85979ead54",
+    "url": "/deezweb/js/search.025e773c.js"
+  },
+  {
+    "revision": "6e159ea9ddbb46e9bf16",
+    "url": "/deezweb/css/track.a6dc7d98.css"
+  },
+  {
+    "revision": "6e159ea9ddbb46e9bf16",
+    "url": "/deezweb/js/track.74c965e9.js"
+  },
+  {
+    "revision": "28ff39586cdfe66d1752708f60609b06",
+    "url": "/deezweb/img/logo.28ff3958.png"
+  },
+  {
+    "revision": "ca4c8bcf5b61daee11d50aa85d822e53",
+    "url": "/deezweb/img/lave.ca4c8bcf.svg"
+  },
+  {
+    "revision": "ddf2d34ceafcd4f40a1aa5ab3be53530",
+    "url": "/deezweb/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deezweb/robots.txt"
+  }
+];
